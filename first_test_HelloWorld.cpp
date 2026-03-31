89f6fa8d39046ab3f9295a631e59cbbf5da92cd4
Daniil <yuutaprod@gmail.com>
2026-04-01 00:02:27 +0300

    Added and printed func of fibonacci numbers

--- first_test_HelloWorld.cpp
+++ first_test_HelloWorld.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 using namespace std;
+int fib(int n){
+    if (n <= 2){
+        return 1;
+    }
+    return fib(n-1) + fib(n-2);
+}
+
 int main(){
-    cout << "Hello World"; 
+    int n; cout << "Insert your n = "; cin >> n;
+    cout << fib(n) << endl;
+    cout << "Hello World";
     return 0;
 }
